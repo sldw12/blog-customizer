5764580a1b97928c234bbb2fc2cbcdabdea6129a
sldw12 <Silent.Hillss@yandex.ru>
2026-09-12 04:13:45 +0300

    лишние изменения

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 // Сообщаем вебпаку, что этот файл использует это изображение.
 import plane from 'src/images/plane.png';
 import { Text } from 'src/ui/text';
@@ -6,7 +8,7 @@ import styles from './Article.module.scss';
 
 export const Article = () => {
 	return (
-		<article className={styles.article}>
+		<article className={clsx(styles.article)}>
 			<Text as='h1' size={45} weight={800} uppercase dynamicLite>
 				Портрет Западной Швейцарии
 			</Text>
